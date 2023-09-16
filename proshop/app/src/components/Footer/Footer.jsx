086eb618b8129f389679c869d39bda6e4c20e501
Farooq Mahmud <mahmud.farooq@acuitybrands.com>
2023-09-16 15:53:33 -0700

    Styled footer.

--- proshop/app/src/components/Footer/Footer.jsx
+++ proshop/app/src/components/Footer/Footer.jsx
@@ -1,19 +1,17 @@
 import React from "react";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
 
 export const Footer = ({ year = new Date().getFullYear() }) => {
   return (
-    <footer data-testid="footer">
-      <p>&copy;{year} ProShop. All rights reserved.</p>
-      <nav>
-        <ul>
-          <li>
-            <a href="/privacy-policy">Privacy Policy</a>
-          </li>
-          <li>
-            <a href="/terms-of-service">Terms of Service</a>
-          </li>
-        </ul>
-      </nav>
-    </footer>
+    <Navbar expand="lg">
+      <footer data-testid="footer">
+        <p>&copy;{year} ProShop. All rights reserved.</p>
+        <Nav className="me-auto">
+          <Nav.Link href="/privacy-policy">Privacy Policy</Nav.Link>
+          <Nav.Link href="/terms-of-service">Terms of Service</Nav.Link>
+        </Nav>
+      </footer>
+    </Navbar>
   );
 };
