74df5a19b7f57583e4f78aad11fc8ec00e70a0c8
Farooq Mahmud <mahmud.farooq@acuitybrands.com>
2023-09-16 11:06:06 -0700

    Header component.

---
+++ proshop/app/src/components/Header/Header.spec.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import Header from "./Header";
+
+describe("Header component", () => {
+  describe("Layout", () => {
+    it("Has heading text", () => {
+      render(<Header></Header>);
+      const heading = screen.queryByRole("heading", { name: "ProShop" });
+      expect(heading).toBeInTheDocument();
+    });
+    it("Has home page link", () => {
+      render(<Header></Header>);
+      const link = screen.queryByRole("link", { name: "Home" });
+      expect(link).toBeInTheDocument();
+    });
+    it("Has login link", () => {
+      render(<Header></Header>);
+      const link = screen.queryByRole("link", { name: "Login" });
+      expect(link).toBeInTheDocument();
+    });
+    it("Has cart link", () => {
+      render(<Header></Header>);
+      const link = screen.queryByRole("link", { name: "Cart" });
+      expect(link).toBeInTheDocument();
+    });
+  });
+});
