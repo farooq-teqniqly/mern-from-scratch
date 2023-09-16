d0ea8f47ea1251830ded6a18fe097a72ef5b937b
Farooq Mahmud <mahmud.farooq@acuitybrands.com>
2023-09-15 21:07:48 -0700

    Add verify step to pipeline.

--- proshop/app/verifyApp.js
+++ proshop/app/verifyApp.js
@@ -1,25 +1,28 @@
-const puppeteer = require("puppeteer");
+import { launch } from "puppeteer";
 
 (async () => {
-  const browser = await puppeteer.launch();
+  const browser = await launch();
   const page = await browser.newPage();
   const url = process.env.AZURE_APP_URL || "http://localhost:3000";
 
   try {
     await page.goto(url, { waitUntil: "domcontentloaded" });
     const content = await page.content();
+    const expectedText = "Learn React";
 
-    if (content.includes("Learn React")) {
-      console.log('Text "Learn React" found on the page. Verification passed.');
+    if (content.includes(expectedText)) {
+      console.log(
+        `Text "${expectedText}" found on the page. Verification passed.`
+      );
     } else {
       console.error(
-        'Text "Learn React" not found on the page. Verification failed.'
+        `Text "${expectedText}" not found on the page. Verification failed.`
       );
-      process.exit(1); // Fail the GitHub Actions workflow
+      process.exit(1);
     }
   } catch (error) {
     console.error("An error occurred during verification:", error);
-    process.exit(1); // Fail the GitHub Actions workflow
+    process.exit(1);
   } finally {
     await browser.close();
   }
